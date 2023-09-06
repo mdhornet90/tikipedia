8971f221a3ef6c6c730fcf4ce2423d801aae2616
Chris <mdhornet90@gmail.com>
2023-09-05 21:26:11 -0500

    Last refactoring, swear to god

--- api/src/graphql/index.ts
+++ api/src/graphql/index.ts
@@ -2,18 +2,7 @@ import { makeExecutableSchema } from '@graphql-tools/schema';
 import { typeDef as Ingredients, resolvers as IngredientResolvers } from './ingredients';
 import { typeDef as Glassware, resolvers as GlasswareResolvers } from './glassware';
 
-const resolvers = {
-  Query: {
-    ...GlasswareResolvers.Query,
-    ...IngredientResolvers.Query,
-  },
-  Mutation: {
-    ...GlasswareResolvers.Mutation,
-    ...IngredientResolvers.Mutation,
-  },
-};
-
 export const schema = makeExecutableSchema({
   typeDefs: [Glassware, Ingredients],
-  resolvers,
+  resolvers: [GlasswareResolvers, IngredientResolvers],
 });
