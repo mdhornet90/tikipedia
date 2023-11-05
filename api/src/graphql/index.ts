bd396f318ab78644f892bdb99b7bc74654c93cdb
Chris <mdhornet90@gmail.com>
2023-11-05 00:49:16 -0500

    Adding garnish graphql infra to top level schema

--- api/src/graphql/index.ts
+++ api/src/graphql/index.ts
@@ -1,9 +1,10 @@
 import { makeExecutableSchema } from '@graphql-tools/schema';
+import { typeDef as Garnishes, resolvers as GarnishResolvers } from './garnish';
 import { typeDef as Glassware, resolvers as GlasswareResolvers } from './glassware';
 import { typeDef as Ingredients, resolvers as IngredientResolvers } from './ingredients';
 import { typeDef as Recipes, resolvers as RecipeResolvers } from './recipes';
 
 export const schema = makeExecutableSchema({
-  typeDefs: [Glassware, Ingredients, Recipes],
-  resolvers: [GlasswareResolvers, IngredientResolvers, RecipeResolvers],
+  typeDefs: [Garnishes, Glassware, Ingredients, Recipes],
+  resolvers: [GarnishResolvers, GlasswareResolvers, IngredientResolvers, RecipeResolvers],
 });
